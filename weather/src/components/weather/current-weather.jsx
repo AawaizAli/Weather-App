3619b1c1402e75caac32d6821a2a698f6ab5db88
Aawaiz Ali <114678240+AawaizAli@users.noreply.github.com>
2024-05-28 14:21:46 +0500

    2nd

--- weather/src/components/weather/current-weather.jsx
+++ weather/src/components/weather/current-weather.jsx
@@ -1,17 +1,27 @@
-import './current-weather.css'
+import "./current-weather.css";
 
 const CurrentWeather = () => {
     return (
         <div className="weather">
-            <div className="img-container">
-                <img src="../../../public/icons/01d.png" alt="weather" className="weather-icon" />
+            
+            <div className="temperature">
+                <p className="number">22</p>
+                <p className="degree-celcius">°</p>
             </div>
-            <div className="top">
-                <p className="city">Karachi</p>
-                <p className="weather-description">Very Hot</p>
+
+            <p className="city">Karachi</p>
+            <div className="bottom-row">
+                <div className="humidity">
+                    <p className="title">Humidity:</p>
+                    <p className="content">55%</p>
+                </div>
+                <div className="feels-like">
+                    <p className="title">Feels Like:</p>
+                    <p className="content">60°</p>
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default CurrentWeather;
