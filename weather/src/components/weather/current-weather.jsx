c4e58cf3a9d7574b4915f72de9e77af9f407c39a
Aawaiz Ali <114678240+AawaizAli@users.noreply.github.com>
2024-05-28 21:11:14 +0500

    fully functional with temperature

--- weather/src/components/weather/current-weather.jsx
+++ weather/src/components/weather/current-weather.jsx
@@ -1,23 +1,30 @@
 import "./current-weather.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
+
+    const celcius = Math.round(data.temperature - 273.15);
+    const fahrenheit = Math.round((data.temperature - 273.15) * 9/5 + 32);
+
+    const celciusFeelsLike = Math.round(data.feelsLike - 273.15);
+    const fahrenheitFeelsLike = Math.round((data.feelsLike - 273.15) * 9/5 + 32);
+
     return (
-        <div className="weather">
-            
+        <div className="weather dm-sans-custom">
+
             <div className="temperature">
-                <p className="number">22</p>
+                <p className="number">{celcius}</p>
                 <p className="degree-celcius">°</p>
             </div>
 
-            <p className="city">Karachi</p>
+            <p className="city">{data.city}</p>
             <div className="bottom-row">
                 <div className="humidity">
                     <p className="title">Humidity:</p>
-                    <p className="content">55%</p>
+                    <p className="content">{data.humidity}%</p>
                 </div>
                 <div className="feels-like">
                     <p className="title">Feels Like:</p>
-                    <p className="content">60°</p>
+                    <p className="content">{celciusFeelsLike}°</p>
                 </div>
             </div>
         </div>
